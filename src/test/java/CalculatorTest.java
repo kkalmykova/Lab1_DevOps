0f1e33654918dfd2bb2bd27f93c227bc6f9d3f49
kkalmykova <kat.ak.2002@gmail.com>
2023-10-30 20:57:04 +0200

    jar files added

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,28 +7,28 @@ public class CalculatorTest {
     public void testCalculate_Addition() {
         Addition addition = new AdditionClass();
         double result = addition.calculate(5, -1.5);
-        assertEquals(3.5, result, 0.0001); // Delta included for comparing floating-point numbers
+        assertEquals(3.5, result, 0.0001);
     }
 
     @Test
     public void testCalculate_Subtraction() {
         Subtraction subtraction = new SubtractionClass();
         double result = subtraction.calculate(5, -1.5);
-        assertEquals(6.5, result, 0.0001); // Delta included for comparing floating-point numbers
+        assertEquals(6.5, result, 0.0001);
     }
 
     @Test
     public void testCalculate_Multiplication() {
         Multiplication multiplication = new MultiplicationClass();
         double result = multiplication.calculate(-2.7, -2.0);
-        assertEquals(5.4, result, 0.0001); // Delta included for comparing floating-point numbers
+        assertEquals(5.4, result, 0.0001);
     }
 
     @Test
     public void testCalculate_Division() {
         Division division = new DivisionClass();
         double result = division.calculate(-18, -2);
-        assertEquals(9.0, result, 0.0001); // Delta included for comparing floating-point numbers
+        assertEquals(9.0, result, 0.0001);
     }
 
     @Test(expected = ArithmeticException.class)
