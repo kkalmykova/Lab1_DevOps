30131839a7cf93e13c4380a8cc5c329369510b60
kkalmykova <kat.ak.2002@gmail.com>
2023-10-30 15:56:11 +0200

    unit tests added

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CalculatorTest {
+
+    @Test
+    public void testCalculate_Addition() {
+        Addition addition = new AdditionClass();
+        double result = addition.calculate(5, -1.5);
+        assertEquals(3.5, result);
+    }
+
+    @Test
+    public void testCalculate_Subtraction() {
+        Subtraction subtraction = new SubtractionClass();
+        double result = subtraction.calculate(5, -1.5);
+        assertEquals(6.5, result); //
+    }
+
+    @Test
+    public void testCalculate_Multiplication() {
+        Multiplication multiplication = new MultiplicationClass();
+        double result = multiplication.calculate(-2.7, -2.0);
+        assertEquals(5.4, result);
+    }
+
+    @Test
+    public void testCalculate_Division() {
+        Division division = new DivisionClass();
+        double result = division.calculate(-18, -2);
+        assertEquals(9.0, result); // Comparison with delta for floating-point numbers
+    }
+
+    @Test
+    public void testCalculate_DivisionByZero() {
+        Division division = new DivisionClass();
+        assertThrows(ArithmeticException.class, () -> {
+            division.calculate(10, 0);
+        });
+    }
+    }
